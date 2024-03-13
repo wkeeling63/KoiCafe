21940adf77f0bf22b8548e9876abf3283f6b3e07
William Keeling <59849219+wkeeling63@users.noreply.github.com>
2024-03-13 12:56:49 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0; url = http://<?php echo gethostname(); ?>/koicafe.php" />
+</head>
+<body>
+   <h1>WELCOME TO KoiCafe</h1>
+   <p>you'll be redirected to KoiCafe page.</p>
+</body>
+</html> 
